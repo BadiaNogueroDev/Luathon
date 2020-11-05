8ebeb8ff9a6f0988bd5d7a39e36a9e2fb2a02ef1
Helnyr <helnyr@users.noreply.github.com>
2020-11-05 18:53:11 +0100

    main menu acabat

--- scripts/menu.lua
+++ scripts/menu.lua
@@ -14,8 +14,10 @@ local buttons = {}
 
 function menu:new()
   background = love.graphics.newImage("sprites/Fons-Paper.png")
+  startButton = love.graphics.newImage("sprites/Start-Button.png")
+  exitButton = love.graphics.newImage("sprites/Exit-Button.png")
   
-  table.insert(buttons, newButton("Start Game", function() 
+  table.insert(buttons, newButton("", function() --Start Game
                         inGame = true
                         startGame() end))
   --[[table.insert(buttons, newButton("Scoreboard", function() 
@@ -24,7 +26,7 @@ function menu:new()
                         scoreboard = true
                         sBoard:printLeaderboard() end))
   ]]--
-  table.insert(buttons, newButton("Exit Game", function() love.event.quit(0) end))
+  table.insert(buttons, newButton("", function() love.event.quit(0) end)) --Exit Game
 end
 
 
@@ -34,15 +36,22 @@ function menu:draw()
   local w = love.graphics.getWidth()
   local h = love.graphics.getHeight()
   
+  local spriteWidth = 1
+  local spriteHeight = 1
   local buttonWidth = w/2.5
-  local buttonHeight = h/12
-  local margin = 15
+  local buttonHeight = h/8
+  
+  local margin = 150
   
   local totalHeight = (buttonHeight + margin) * #buttons
+  local totalSpriteHeight = (spriteHeight + margin) * #buttons
   local cursorY = 0
   
   for i, button in ipairs(buttons) do
     button.last = button.now
+    
+    local spriteX = w/2 - startButton:getWidth()/2
+    local spriteY = h/2 - totalSpriteHeight/2 + cursorY
     
     local buttonX = w/2 - buttonWidth/2
     local buttonY = h/2 - totalHeight/2 + cursorY
@@ -52,7 +61,7 @@ function menu:draw()
     --Highlight
     local highlight = mX > buttonX and mX < buttonX + buttonWidth and mY > buttonY and mY < buttonY + buttonHeight
     if highlight then
-      color = {0.8, 0.8, 0.9}
+      color = {0.8, 0.8, 0.9, 0.2}
     end
     
     --Detectar un solo clic
@@ -62,7 +71,13 @@ function menu:draw()
     end
     
     love.graphics.setColor(unpack(color))
-    love.graphics.rectangle("fill", buttonX, buttonY, buttonWidth, buttonHeight)
+    if i == 1 then
+      love.graphics.draw(startButton, spriteX, buttonY)
+    else
+      love.graphics.draw(exitButton, spriteX, buttonY)
+    end
+  
+    --love.graphics.rectangle("fill", buttonX, buttonY, buttonWidth, buttonHeight)
     
     love.graphics.setColor(0, 0, 0)
     
